f44527ec266294a00c49f4c23fc67b8d048d0086
stefanaciudin <stefanaciudin@gmail.com>
2023-12-24 13:58:01 +0200

    added project - first version

---
+++ project.py
@@ -0,0 +1,56 @@
+import requests
+import folium
+import json
+
+
+def get_photos(tag, count, output_file="outputdata.json"):
+    url = 'https://api.flickr.com/services/rest/'
+    params = {
+        'method': 'flickr.photos.search',
+        'api_key': 'a7d50a45916d6f2daaf0eaa86801addf',
+        'tags': tag,
+        'format': 'json',
+        'nojsoncallback': 1,
+        'per_page': count * 100,
+        'sort': 'date-posted-desc',
+        'extras': 'geo'
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    if output_file:
+        with open(output_file, 'w') as f:
+            json.dump(data, f)
+
+    photos = data['photos']['photo']
+
+    filtered_photos = [photo for photo in photos if 'latitude' in photo and 'longitude' in photo]
+    placed = 0
+    _map = folium.Map(location=[0, 0], zoom_start=2)
+
+    coordinate_counter = {}
+    for photo in filtered_photos:
+        if placed == count:
+            break
+        coordinate = (photo['latitude'], photo['longitude'])
+        if photo['latitude'] != 0 and photo['longitude'] != 0:
+            if coordinate not in coordinate_counter:
+                coordinate_counter[coordinate] = 1
+            else:
+                coordinate_counter[coordinate] += 1
+
+            marker_popup = f"Coordinates: {coordinate}<br>Photo IDs: {', '.join(str(photo['id']) for photo in filtered_photos if (photo['latitude'], photo['longitude']) == coordinate)}"
+            folium.Marker(
+                location=[float(photo['latitude']), float(photo['longitude'])],
+                popup=marker_popup
+            ).add_to(_map)
+            print(f"{placed}: Placed photo with id:{photo['id']}, coordinates: {coordinate}")
+            placed += 1
+
+    return _map
+
+
+t = 'stars'
+cnt = 7
+
+generated_map = get_photos(t, cnt)
+generated_map.save('map.html')
